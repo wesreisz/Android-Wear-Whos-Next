6ca676352629956ef2fed43d09784422f5edf55f
Wesley Reisz <wes@wesleyreisz.com>
2014-10-19 19:26:19 -0400

    added api

---
+++ mongo/auburn-team.js
@@ -0,0 +1,118 @@
+{
+	"team" : "Auburn Tigers",
+	"nickname" : "Tigers",
+	"warcry" : "Go Tigers!!!",
+	"hometown" : "Auburn, Alabama",
+	"logo" : "louisville.png",
+	"helmet" : "louisville_helmet.png",
+	"schedule" :[
+	    {
+		    "team" : "Arkansaas Razerbacks",
+		    "nickname" : "Razerbacks",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-08-30"),
+			"time" : "4:00PM",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "San Jose State Spartans",
+		    "nickname" : "Spartans",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-06"),
+			"time" : "7:00PM",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "Kansas State Wildcats",
+		    "nickname" : "Cats",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-18"),
+			"time" : "7:30PM",
+			"location" : "@Kansas State"
+	    },
+		{
+		    "team" : "Louisiana Tech Bulldogs",
+		    "nickname" : "Bulldogs",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-27"),
+			"time" : "4:00PM",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "LSU",
+		    "nickname" : "Tigers",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-04"),
+			"time" : "7:00PM",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "Mississipi State Bulldogs",
+		    "nickname" : "Bulldogs",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-11"),
+			"time" : "3:30PM",
+			"location" : "@Mississippi State"
+	    },
+		{
+		    "team" : "South Carolina Gamecocks",
+		    "nickname" : "Gamecocks",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-25"),
+			"time" : "TBD",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "Ole Miss Rebels",
+		    "nickname" : "Rebels",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-1"),
+			"time" : "3:30PM",
+			"location" : "@Ole Miss"
+	    },
+		{
+		    "team" : "Texas A&M Aggies",
+		    "nickname" : "Aggies",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-8"),
+			"time" : "TBD",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "Georgia Bulldogs",
+		    "nickname" : "Dawgs",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-15"),
+			"time" : "TBD",
+			"location" : "@Georgia"
+	    },
+		{
+		    "team" : "Samford Bulldogs",
+		    "nickname" : "Bulldogs",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-22"),
+			"time" : "TBD",
+			"location" : "@Auburn"
+	    },
+		{
+		    "team" : "Alabama Crimson Tide",
+		    "nickname" : "Tide",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-29"),
+			"time" : "TBD",
+			"location" : "@Alabama"
+	    }
+	]
+}
---
+++ mongo/cards-team.js
@@ -0,0 +1,118 @@
+{
+	"team" : "Louisville Cardinals",
+	"nickname" : "Cards",
+	"warcry" : "Go Cards!!!",
+	"hometown" : "Louisville, Kentucky",
+	"logo" : "louisville.png",
+	"helmet" : "louisville_helmet.png",
+	"schedule" :[
+	    {
+		    "team" : "Miami Hurricanes",
+		    "nickname" : "Canes",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-01"),
+			"time" : "8:00PM",
+			"location" : "@Louisville"
+	    },
+		{
+		    "team" : "Murray State Racers",
+		    "nickname" : "Murray State",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-06"),
+			"time" : "7:00PM",
+			"location" : "@Louisville"
+	    },
+		{
+		    "team" : "Virginia",
+		    "nickname" : "Virginia Cavaliers",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-13"),
+			"time" : "12:30PM",
+			"location" : "@Virginia"
+	    },
+		{
+		    "team" : "FIU Goldern Panthers",
+		    "nickname" : "FIU",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-20"),
+			"time" : "3:30PM",
+			"location" : "@FIU"
+	    },
+		{
+		    "team" : "Wake Forest",
+		    "nickname" : "Wake Forest",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-09-27"),
+			"time" : "3:30PM",
+			"location" : "@Louisville"
+	    },
+		{
+		    "team" : "Syracuse Orance",
+		    "nickname" : "Cuse",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-03"),
+			"time" : "7:00PM",
+			"location" : "@Syracuse"
+	    },
+		{
+		    "team" : "Clemson Tigers",
+		    "nickname" : "Tigers",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-11"),
+			"time" : "3:30PM",
+			"location" : "@Clemson"
+	    },
+		{
+		    "team" : "NC State Wolfpack",
+		    "nickname" : "Wolfpack",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-18"),
+			"time" : "3:30PM",
+			"location" : "@Louisville"
+	    },
+		{
+		    "team" : "Florida State Seminoles",
+		    "nickname" : "Noles",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-10-30"),
+			"time" : "7:30PM",
+			"location" : "@Louisville"
+	    },
+		{
+		    "team" : "Boston College Eagles",
+		    "nickname" : "BC",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-01"),
+			"time" : "TBD",
+			"location" : "@Boston College"
+	    },
+		{
+		    "team" : "Notre Dame Fighting Irish",
+		    "nickname" : "Fighting Irish",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-22"),
+			"time" : "3:30PM",
+			"location" : "@Notre Dame"
+	    },
+		{
+		    "team" : "Kentucky Wildcates",
+		    "nickname" : "Cats",
+		    "logo" : "louisville.png",
+			"helmet" : "louisville_helmet.png",
+			"date" : new Date("2014-11-29"),
+			"time" : "TBD",
+			"location" : "@Louisville"
+	    }
+	]
+}
