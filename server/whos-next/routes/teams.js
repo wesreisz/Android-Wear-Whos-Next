0f965e49b6b3f77c97d3508bb1e0f3117c41b2b6
Wesley Reisz <wes@wesleyreisz.com>
2015-04-26 15:18:44 -0400

    updated server for mongoskin

--- server/whos-next/routes/teams.js
+++ server/whos-next/routes/teams.js
@@ -1,50 +1,47 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET team listing. */
 router.get('/', function(req, res) {
-  	var db = req.db;
-        db.collection('teamlist').find({},{'schedule':0}).sort({'team':1}).toArray(function (err, items) {
-        res.json(items);
+    var db = req.db;
+    db.collection('teamlist').find({},{'schedule':0}).sort({'team':1}).toArray(function (err, items) {
+       res.json(items);
     });
 });
-
 /* GET team listing for a single school by id... include schedule. */
 router.get('/:id', function(req, res) {
-  	var db = req.db;
-    var id = req.params.id;
-    db.collection('teamlist').findById(id,function (err, items) {
-        res.json(items);
-    });
+   var db = req.db;
+   var id = req.params.id;
+   db.collection('teamlist').findById(id, function(err, items) {
+      res.json(items);
+   });
 });
-
 /* GET team listing for a single school by nickname... include schedule. */
 router.get('/nickname/:nickname', function(req, res) {
-  	var db = req.db;
+    var db = req.db;
     var schoolNickname = req.params.nickname;
     db.collection('teamlist').findOne({'nickname':schoolNickname},function (err, items) {
         res.json(items);
     });
 });
 
-/* GET next game by nickname */
+/* GET next game by id */
 router.get('/:id/next', function(req, res) {
-  	var db = req.db;
-    var id = req.params.id;
-   	db.collection('teamlist').findById(id, {schedule: {$elemMatch: { date : {$gte:new Date()}} }}, function (err, items) {
+   var db = req.db;
+   var objId = req.params.id;
+   var ObjectID = require('mongoskin').ObjectID;
+   db.collection('teamlist').findOne({'_id' : ObjectID.createFromHexString(objId), 'schedule': {$elemMatch: { date : {$gte:new Date()}} }},function (err, items) {
         res.json(items);
     });
 });
 
 /* GET next game by nickname */
 router.get('/nickname/:nickname/next', function(req, res) {
-  	var db = req.db;
+    var db = req.db;
     var schoolNickname = req.params.nickname;
-   	db.collection('teamlist').findOne({'nickname':schoolNickname}, {schedule: {$elemMatch: { date : {$gte:new Date()}} }}, function (err, items) {
+    db.collection('teamlist').findOne({'nickname' : schoolNickname, 'schedule': {$elemMatch: { date : {$gte:new Date()}} }},function (err, items) {
         res.json(items);
     });
 });
-
 
 
 module.exports = router;
